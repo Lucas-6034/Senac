663351c4c89daa8d006b543b1a4dfb6f9d83d932
Lucas Camilo <lucas.6034@df.senac.br>
2025-10-24 11:24:34 -0300

    demonstração

--- JS - Lógica de Programação/Aula 4/Parte 15/script.js
+++ JS - Lógica de Programação/Aula 4/Parte 15/script.js
@@ -36,3 +36,8 @@ setTimeout(function(){
 // RODANDO UM CÓDIGO APÓS 6 SEGUNDOS, APENAS UMA VEZ
 // Somente para exibir uma mensagem separada (esse código vai rodar antes do código acima).
 setTimeout(function(){console.log('Olá Mundo!');}, 6000);
+
+
+
+
+a
